d29e5834023e8b6eaa96efc035fc103fb598d0d0
PierreLgol <plgol.perso@gmail.com>
2026-02-07 12:28:29 +0100

    updating the code to fit the real imu shm format

--- src/common.zig
+++ src/common.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 const Io = std.Io;
 
-pub const ImuShmSchema = packed struct(u192) {
+pub const Sample = packed struct(u160) {
     timestamp: i64 = 0,
     accel_x: i16 = 0,
     accel_y: i16 = 0,
@@ -10,24 +10,22 @@ pub const ImuShmSchema = packed struct(u192) {
     gyro_x: i16 = 0,
     gyro_y: i16 = 0,
     gyro_z: i16 = 0,
-    elevation: i16 = 0,
-    bearing: i16 = 0,
 
-    pub const init: ImuShmSchema = .{};
+    pub const init: Sample = .{};
 
-    pub fn fromBytes(bytes: [@sizeOf(ImuShmSchema)]u8) ImuShmSchema {
-        return std.mem.bytesToValue(ImuShmSchema, bytes[0..]);
+    pub fn fromBytes(bytes: [@sizeOf(Sample)]u8) Sample {
+        return std.mem.bytesToValue(Sample, bytes[0..]);
     }
 
-    pub fn asBytes(self: *ImuShmSchema) []u8 {
+    pub fn asBytes(self: *Sample) []u8 {
         return std.mem.asBytes(self);
     }
 
-    pub fn deserialize(reader: *Io.Reader, endian: std.builtin.Endian) error{ ReadFailed, EndOfStream }!ImuShmSchema {
+    pub fn deserialize(reader: *Io.Reader, endian: std.builtin.Endian) error{ ReadFailed, EndOfStream }!Sample {
         return reader.takeStruct(@This(), endian);
     }
 
-    pub fn serialize(self: *const ImuShmSchema, writer: *Io.Writer, endian: std.builtin.Endian) error{WriteFailed}!void {
+    pub fn serialize(self: *const Sample, writer: *Io.Writer, endian: std.builtin.Endian) error{WriteFailed}!void {
         return writer.writeStruct(self.*, endian);
     }
 
@@ -35,7 +33,7 @@ pub const ImuShmSchema = packed struct(u192) {
         self: @This(),
         writer: *std.Io.Writer,
     ) std.Io.Writer.Error!void {
-        const formatter: std.json.Formatter(ImuShmSchema) = .{
+        const formatter: std.json.Formatter(Sample) = .{
             .value = self,
             .options = .{},
         };
@@ -43,50 +41,129 @@ pub const ImuShmSchema = packed struct(u192) {
     }
 
     comptime {
-        if (@bitSizeOf(ImuShmSchema) != 192) @compileError("Invalid size");
+        if (@bitSizeOf(Sample) != 160) @compileError("Invalid size");
+    }
+};
+
+pub const Report = packed struct(u192) {
+    sample: Sample = .init,
+    elevation: i16 = 0,
+    bearing: i16 = 0,
+
+    pub const init: Report = .{};
+
+    pub fn fromBytes(bytes: [@sizeOf(Report)]u8) Report {
+        return std.mem.bytesToValue(Report, bytes[0..]);
+    }
+
+    pub fn asBytes(self: *Report) []u8 {
+        return std.mem.asBytes(self);
+    }
+
+    pub fn deserialize(reader: *Io.Reader, endian: std.builtin.Endian) error{ ReadFailed, EndOfStream }!Report {
+        return reader.takeStruct(@This(), endian);
+    }
+
+    pub fn serialize(self: *const Report, writer: *Io.Writer, endian: std.builtin.Endian) error{WriteFailed}!void {
+        return writer.writeStruct(self.*, endian);
+    }
+
+    pub fn format(
+        self: @This(),
+        writer: *std.Io.Writer,
+    ) std.Io.Writer.Error!void {
+        const formatter: std.json.Formatter(Report) = .{
+            .value = self,
+            .options = .{},
+        };
+        try formatter.format(writer);
+    }
+
+    comptime {
+        if (@bitSizeOf(Report) != 192) @compileError("Invalid size");
     }
 };
 
 const testing = std.testing;
 
-test "ImuShmSchema - imuShmSchemafromBytes" {
-    const bytes: [@sizeOf(ImuShmSchema)]u8 = @splat(0);
-    const expected: ImuShmSchema = .init;
-    const actual = ImuShmSchema.fromBytes(bytes);
+test "Sample - fromBytes" {
+    const bytes: [@sizeOf(Sample)]u8 = @splat(0);
+    const expected: Sample = .init;
+    const actual = Sample.fromBytes(bytes);
     try testing.expectEqual(expected, actual);
 }
 
-test "ImuShmSchema - bytesFromImuShmSchema" {
-    var self: ImuShmSchema = .init;
-    const expected: [@sizeOf(ImuShmSchema)]u8 = @splat(0);
-    const actual = ImuShmSchema.asBytes(&self);
+test "Sample - asBytes" {
+    var self: Sample = .init;
+    const expected: [@sizeOf(Sample)]u8 = @splat(0);
+    const actual = Sample.asBytes(&self);
     try testing.expectEqualSlices(u8, &expected, actual);
 }
 
-test "ImuShmSchema - fromReader" {
-    var expected: ImuShmSchema = .init;
+test "Sample - deserialize" {
+    var expected: Sample = .init;
     var fixed_reader: Io.Reader = .fixed(expected.asBytes());
     const reader: *Io.Reader = &fixed_reader;
-    const actual = ImuShmSchema.deserialize(reader, builtin.cpu.arch.endian()) catch unreachable;
+    const actual = Sample.deserialize(reader, builtin.cpu.arch.endian()) catch unreachable;
     try testing.expectEqual(expected, actual);
 }
 
-test "ImuShmSchema - toWriter" {
-    var self: ImuShmSchema = .init;
-    var expected: [@sizeOf(ImuShmSchema)]u8 = @splat(0);
+test "Sample - serialize" {
+    var self: Sample = .init;
+    var expected: [@sizeOf(Sample)]u8 = @splat(0);
     var fixed_writer: Io.Writer = .fixed(&expected);
     const writer: *Io.Writer = &fixed_writer;
     self.serialize(writer, builtin.cpu.arch.endian()) catch unreachable;
-    try testing.expect(writer.unusedCapacityLen() == 8);
     try testing.expectEqualSlices(u8, self.asBytes(), writer.buffer);
 }
 
-test "ImuShmSchema - format" {
+test "Sample - format" {
     var actual: [256]u8 = @splat(0);
     var fixed_writer: Io.Writer = .fixed(&actual);
     const writer: *Io.Writer = &fixed_writer;
-    const self: ImuShmSchema = .init;
+    const self: Sample = .init;
     try writer.print("{f}", .{self});
-    const expected = "{\"timestamp\":0,\"accel_x\":0,\"accel_y\":0,\"accel_z\":0,\"gyro_x\":0,\"gyro_y\":0,\"gyro_z\":0,\"elevation\":0,\"bearing\":0}";
+    const expected = "{\"timestamp\":0,\"accel_x\":0,\"accel_y\":0,\"accel_z\":0,\"gyro_x\":0,\"gyro_y\":0,\"gyro_z\":0}";
+    try testing.expectEqualSlices(u8, expected, actual[0..expected.len]);
+}
+
+test "Report - fromBytes" {
+    const bytes: [@sizeOf(Report)]u8 = @splat(0);
+    const expected: Report = .init;
+    const actual = Report.fromBytes(bytes);
+    try testing.expectEqual(expected, actual);
+}
+
+test "Report - asBytes" {
+    var self: Report = .init;
+    const expected: [@sizeOf(Report)]u8 = @splat(0);
+    const actual = Report.asBytes(&self);
+    try testing.expectEqualSlices(u8, &expected, actual);
+}
+
+test "Report - deserialize" {
+    var expected: Report = .init;
+    var fixed_reader: Io.Reader = .fixed(expected.asBytes());
+    const reader: *Io.Reader = &fixed_reader;
+    const actual = Report.deserialize(reader, builtin.cpu.arch.endian()) catch unreachable;
+    try testing.expectEqual(expected, actual);
+}
+
+test "Report - serialize" {
+    var self: Report = .init;
+    var expected: [@sizeOf(Report)]u8 = @splat(0);
+    var fixed_writer: Io.Writer = .fixed(&expected);
+    const writer: *Io.Writer = &fixed_writer;
+    self.serialize(writer, builtin.cpu.arch.endian()) catch unreachable;
+    try testing.expectEqualSlices(u8, self.asBytes(), writer.buffer);
+}
+
+test "Report - format" {
+    var actual: [256]u8 = @splat(0);
+    var fixed_writer: Io.Writer = .fixed(&actual);
+    const writer: *Io.Writer = &fixed_writer;
+    const self: Report = .init;
+    try writer.print("{f}", .{self});
+    const expected = "{\"sample\":{\"timestamp\":0,\"accel_x\":0,\"accel_y\":0,\"accel_z\":0,\"gyro_x\":0,\"gyro_y\":0,\"gyro_z\":0},\"elevation\":0,\"bearing\":0}";
     try testing.expectEqualSlices(u8, expected, actual[0..expected.len]);
 }
